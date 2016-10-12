82dc9f1b5a70c38cd2fe7e462a612c98aa51bb79
Andrew Arrow <andrew@0x7a69.net>
2016-10-12 00:31:05 +0000

    sql

--- users/index.php
+++ users/index.php
@@ -6,6 +6,22 @@
 <p><a href="/index.php">Mall Manager v1.0</a>: List Users</p>
 
 
+<table>
+<? 
+    $conn = mysqli_connect("localhost", "root", "root", "mallmanager");
+    $query = mysqli_query($conn, "select * from users");
+    while ($row = mysqli_fetch_array($query)) {
+?> 
+
+<tr>
+<td><?= $row['id'] ?>.</td>
+<td><?= $row['firstname'] ?></td>
+<td><?= $row['lastname'] ?></td>
+</tr>
+
+<? } ?>
+</table>
+
 
 </div>
 
