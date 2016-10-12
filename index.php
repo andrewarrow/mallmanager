781fa021584d98d993519d6c9702fab1e8513eae
Andrew Arrow <andrew@0x7a69.net>
2016-10-12 00:17:59 +0000

    start of mall manager project

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<div style="padding: 10px; font-family: sans-serif; font-size: 18px;">
+
+<p>Mall Manager v1.0</p>
+
+<ol>
+<li><a href="/users">List Users</a></li>
+<li>List Stores</li>
+</ol>
+
+
+</div>
+
+</body>
+</html>
+
---
+++ users/index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<div style="padding: 10px; font-family: sans-serif; font-size: 18px;">
+
+<p><a href="/index.php">Mall Manager v1.0</a>: List Users</p>
+
+
+
+</div>
+
+</body>
+</html>
+
